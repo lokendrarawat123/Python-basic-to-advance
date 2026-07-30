ce2af3a5adee8e5c9f29abeae9e619a371eb960d
lokendra123 <lokey973@gmail.com>
2026-07-30 09:20:33 +0545

    add: implement remainder calculation and add circle area script; create ReadMe with Python topics roadmap

---
+++ Basic Python/task/operator/cerclearea.py
@@ -0,0 +1,5 @@
+# Take the radius of a circle and calculate its area.
+radius=float(input("enter the radius of a circle"))
+pi=3.14
+area=pi*radius**2
+print("ther radius of the circle is = ",area)
--- Basic Python/task/operator/remainder.py
+++ Basic Python/task/operator/remainder.py
@@ -1,1 +1,5 @@
 # Find the remainder when one number is divided by another.
+first_number=int(input("enter the first number"))
+second_number=int(input("enter second number for divided"))
+remainder=first_number%second_number
+print("the remainder is = ",remainder)
